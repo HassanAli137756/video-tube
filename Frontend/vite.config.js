cac1b4057fcb96c4e1e347f3bdc536e993c726a5
Hassan Ali <hassanali240489@gmail.com>
2026-08-31 12:35:39 -0700

    updated project

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  
-  server:
-  {
-    
-    proxy:
-    {
-      '/api/v1': 'https://video-tube-34ly-50kcudvw8-hassanali240489-4130s-projects.vercel.app/'
-    }
-  },
+ 
   plugins: [
     tailwindcss(),
   ],
